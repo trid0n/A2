9c1439830e3bff6884695fdf3abd3b7223718586
Liam Winfield <liambird3@gmail.com>
2023-09-19 23:18:29 +1000

    added footer css

--- src/modules/Footer/Footer.js
+++ src/modules/Footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>
-      {/* Your footer content */}
-      </div>
+    <div className='footer'>
+          <div className="footer-sections">
+          <div className="contact-us">
+            <div>
+            <b>Contact us</b> {<br />}
+            Phone: 0123456789 {<br />}
+            Address: 12 Melbourne street, 2930 Melbourne {<br />}
+            Email: mba@gmail.com
+          </div>
+          </div>
+          <section>
+            <div className="footer-heading">
+          <h1>youtube</h1>
+            </div>
+          </section>
+          <section>
+            <div className="footer-heading">
+          <h1>facebook</h1>
+            </div>
+          </section>
+          <section>
+            <div className="footer-heading">
+          <h1>instagram</h1>
+            </div>
+          </section>
+        </div>
+        </div>
+        
   );
 }
 
