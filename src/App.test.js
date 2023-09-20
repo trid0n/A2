0c72addd5044b362ce6127ed3a6b5da269fecbfc
Liam Winfield <liambird3@gmail.com>
2023-09-20 16:27:19 +1000

    added jest and a test in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import Main from './modules/Main/Main'; // Import your component
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Main', () => {
+  it('renders a greeting message', () => {
+    // Render the Main component
+    const { getByText } = render(<Main />);
+
+    // Check if the greeting message is present in the rendered component
+    const greetingMessage = getByText('I LOVE FOOD');
+    expect(greetingMessage).toBeInTheDocument();
+  });
 });
